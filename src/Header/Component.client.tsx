72c674bfa597f2f7ee7ef03373345db22a70ccdf
Wais <95wais@gmail.com>
2025-01-11 17:35:04 -0500

    fixed font problems in the header

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -55,10 +55,23 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
       {/* Main header section with background and text styling */}
       <div className="py-8 w-full h-[250px] bg-fixed flex flex-col bg-headerFooterColor text-headerFooterText p-[40px]">
         {/* Logo Section - Contains site logo with link to home */}
-        <div className="flex self-center">
-          <Link href="/">
-            <Logo loading="eager" priority="high"  />
-          </Link>
+        <div className="flex items-end justify-center text-center text-white">
+          {/* Logo Section */}
+          <div className="mr-4 mb-[-5px]">
+            <Link href="/">
+              <Logo loading="eager" priority="high" />
+            </Link>
+          </div>
+
+          {/* Text Section */}
+          <div className="flex flex-col">
+            <h1 className="text-5xl leading-[80px] text-center">﷽</h1>
+            <h1 className="text-3xl lg:text-[2rem]">الجمعية الاسلامية لمقاطعة سكولكل</h1>
+            <h1 className="text-xl lg:text-[1.4rem]">Islamic Society of Schuylkill County</h1>
+            <h6 className="text-sm lg:text-[0.80rem]">
+              Promoting Peace through the teachings of Prophet Muhammad
+            </h6>
+          </div>
         </div>
         {/* Navigation Section - Positioned at bottom using flex layout */}
         <div className="flex-1 flex items-end">
@@ -68,3 +81,5 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
     </header>
   )
 }
+
+
